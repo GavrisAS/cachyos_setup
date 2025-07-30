ca80cc5a9b5924a97a5bbd00287c8e44de5d74a2
Alexander Gavris <GavrisAS@gmail.com>
2025-07-30 10:01:26 +0500

    Update setup.fish

--- setup.fish
+++ setup.fish
@@ -39,6 +39,7 @@ ansible-galaxy collection install kewlfft.aur
 echo "Запускаем ansible-pull из $GIT_REPO, плейбук: $PLAYBOOK ..."
 ansible-pull \
     -U $GIT_REPO \
+    -i localhost,(hostname), \
     $PLAYBOOK \
     --ask-become-pass
 
