43b5c01bf38e87544793f26d5b4eb931f2c42efe
Alexander Gavris <GavrisAS@gmail.com>
2025-08-06 01:08:14 +0500

    Update setup.fish

--- setup.fish
+++ setup.fish
@@ -45,6 +45,6 @@ ansible-galaxy collection install kewlfft.aur
 
 # 4) Запускаем плейбук
 echo "Запускаем ansible-pull из $GIT_REPO, плейбук: $PLAYBOOK ..."
-ansible-pull -U $GIT_REPO $PULL_ARGS $PLAYBOOK
+ansible-pull -U $GIT_REPO $PLAYBOOK $PULL_ARGS
 
 echo "✅ Готово! Система настроена."
